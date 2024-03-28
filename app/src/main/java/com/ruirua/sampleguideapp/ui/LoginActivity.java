23e940fbc4eaf917b0bb700c427fb4b68013063b
José Miguel Barbosa <79376027+zeisalone@users.noreply.github.com>
2024-03-28 10:38:57 +0000

    improvement to LoginActivity

--- app/src/main/java/com/ruirua/sampleguideapp/ui/LoginActivity.java
+++ app/src/main/java/com/ruirua/sampleguideapp/ui/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ruirua.sampleguideapp.R;
 public class LoginActivity extends Activity {
@@ -16,6 +18,7 @@ public class LoginActivity extends Activity {
 
         Button buttonLoginVoltar = findViewById(R.id.buttonLoginVoltar);
         Button buttonLoginParaRegister = findViewById(R.id.buttonLoginParaRegister);
+        Button buttonLoginLogin = findViewById(R.id.buttonLoginLogin);
 
         buttonLoginVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,23 @@ public class LoginActivity extends Activity {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        buttonLoginLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextUsername = findViewById(R.id.editLoginTextUsername);
+                EditText editTextPassword = findViewById(R.id.editLoginTextPassword);
+
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (username.equals("teste") && password.equals("teste")) {
+                    Toast.makeText(LoginActivity.this, "Login Feito", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Dados Incorretos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }

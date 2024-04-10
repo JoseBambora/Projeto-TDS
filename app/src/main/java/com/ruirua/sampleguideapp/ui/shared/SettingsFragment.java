3f754902c314f6b1cc3b7f50dab5092ffc5c103e
unknown <jmbarbosa2002@gmail.com>
2024-04-10 17:22:59 +0100

    Slight Changes to Settings

--- app/src/main/java/com/ruirua/sampleguideapp/ui/shared/SettingsFragment.java
+++ app/src/main/java/com/ruirua/sampleguideapp/ui/shared/SettingsFragment.java
@@ -1,14 +1,19 @@
 package com.ruirua.sampleguideapp.ui.shared;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.fragment.app.Fragment;
+import com.ruirua.sampleguideapp.R;
+import com.ruirua.sampleguideapp.ui.utils.UIFuns;
+
 import androidx.fragment.app.Fragment;
 import com.ruirua.sampleguideapp.R;
 import com.ruirua.sampleguideapp.ui.utils.UIFuns;
@@ -33,15 +38,17 @@ public class SettingsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         setOnClicks(view);
-        RadioGroup radioGroupMode = view.findViewById(R.id.radioGroupMode);
-        radioGroupMode.setOnCheckedChangeListener((group, checkedId) -> {
-            RadioButton radioButton = view.findViewById(checkedId);
-            if (radioButton != null) {
-                String mode = radioButton.getText().toString();
-                Toast.makeText(getContext(), "Modo selecionado: " + mode, Toast.LENGTH_SHORT).show();
+
+        ToggleButton toggleButtonMode = view.findViewById(R.id.toggleButtonMode);
+        toggleButtonMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // Modo escuro ativado
+                Toast.makeText(getContext(), "Modo escuro ativado", Toast.LENGTH_SHORT).show();
+            } else {
+                // Modo escuro desativado
+                Toast.makeText(getContext(), "Modo escuro desativado", Toast.LENGTH_SHORT).show();
             }
         });
-
 
         return view;
     }

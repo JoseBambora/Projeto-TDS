845288ec1c9b9a1a9625ab04eb0d5e8b149becc0
Miguel Cidade Silva <73348796+MiguelCidadeSilva@users.noreply.github.com>
2024-04-19 19:45:05 +0100

    Changed the code to make it match the backend

--- app/src/main/java/com/ruirua/sampleguideapp/ui/trails/TrailFragment.java
+++ app/src/main/java/com/ruirua/sampleguideapp/ui/trails/TrailFragment.java
@@ -2,6 +2,7 @@ package com.ruirua.sampleguideapp.ui.trails;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ruirua.sampleguideapp.R;
 import com.ruirua.sampleguideapp.model.pins.Pin;
 import com.ruirua.sampleguideapp.model.trails.Trail;
+import com.ruirua.sampleguideapp.model.user.LoginData;
+import com.ruirua.sampleguideapp.viewModel.PinsViewModel;
+import com.ruirua.sampleguideapp.viewModel.TrailsViewModel;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -25,12 +30,12 @@ import java.util.List;
 
 public class TrailFragment extends Fragment {
 
-    private Trail trail;
+    private int trailId;
 
     private FragmentManager fragmentManager;
 
-    public TrailFragment(Trail trail,FragmentManager fragmentManager) {
-        this.trail = trail;
+    public TrailFragment(int trail, FragmentManager fragmentManager) {
+        this.trailId = trail;
         this.fragmentManager = fragmentManager;
     }
 
@@ -41,35 +46,29 @@ public class TrailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_trail, container, false);
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        observeTrail(view);
+    }
 
+    private void observeTrail(View view) {
+        TrailsViewModel tvm = new ViewModelProvider(this).get(TrailsViewModel.class);
+        tvm.getTrail(trailId).observe(getViewLifecycleOwner(), trail -> {
+            if (trail != null) {
+                populateTrailInfo(trail, view);
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void populateTrailInfo(Trail trail, View view) {
         TextView id = view.findViewById(R.id.idTrilho);
         id.setText("ID: " + trail.getId());
 
         ImageView trailImg = view.findViewById(R.id.trailImg);
-
-        if (trail.getTrailImg() != null && !trail.getTrailImg().isEmpty()) {
-            // Replace "http://" with "https://"
-            String imageUrl = trail.getTrailImg().replace("http:", "https:");
-            Picasso.get().load(imageUrl).into(trailImg, new Callback() {
-                @Override
-                public void onSuccess() {
-                    // Image loaded successfully
-                }
-
-                @Override
-                public void onError(Exception e) {
-                    // Error loading image
-                    trailImg.setVisibility(View.GONE);
-                }
-            });
-        } else {
-            // Image URL is null or empty
-            trailImg.setVisibility(View.GONE);
-        }
+        Log.d("IMAGE", trail.getTrailImg());
+        loadTrailImage(trail.getTrailImg(), trailImg);
 
         TextView trailName = view.findViewById(R.id.trailName);
         trailName.setText(trail.getTrailName());
@@ -83,11 +82,30 @@ public class TrailFragment extends Fragment {
         TextView trailDifficulty = view.findViewById(R.id.trailDifficulty);
         trailDifficulty.setText("Dificuldade: " + trail.getTrailDifficulty());
 
-        // RecyclerView for pins
+        setupTrailPinsRecyclerView(trail.getPins(), view);
+    }
+
+    private void loadTrailImage(String imageUrl, ImageView trailImg) {
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            // Replace "http://" with "https://"
+            imageUrl = imageUrl.replace("http:", "https:").replace("/trail","");
+
+            Log.d("IMAGE", imageUrl);
+            Picasso.get().load(imageUrl).into(trailImg);
+        } else {
+            // Image URL is null or empty
+            if (trailImg != null) {
+                trailImg.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void setupTrailPinsRecyclerView(List<Pin> pinList, View view) {
         RecyclerView recyclerViewPins = view.findViewById(R.id.recycler_view_pins);
         recyclerViewPins.setLayoutManager(new LinearLayoutManager(requireContext()));
-        List<Pin> pinList = trail.getPins();
-        TrailPinRecyclerView adapter = new TrailPinRecyclerView(pinList,fragmentManager);
+        TrailPinRecyclerView adapter = new TrailPinRecyclerView(pinList, fragmentManager);
         recyclerViewPins.setAdapter(adapter);
     }
+
 }
+

b3240cc8961cbe9e8e741053bae5d3deafa2fc5d
Miguel Cidade Silva <73348796+MiguelCidadeSilva@users.noreply.github.com>
2024-05-25 22:50:56 +0100

    Fixed a minor issue

--- BraGuiaReact/src/views/trails/TrailsScreen.jsx
+++ BraGuiaReact/src/views/trails/TrailsScreen.jsx
@@ -3,31 +3,36 @@ import { SafeAreaView, StatusBar, ActivityIndicator } from 'react-native';
 import Trails from '../../components/Trails';
 import { GetTrails } from '../../repositories/Trails';
 import trailsScreenStyles from '../../styles/TrailsScreen';
-import { IsAuthenticated } from '../../redux/SessionState';
+import { IsAuthenticated } from '../../repositories/User';
 import UnauthenticatedScreen from '../UnauthenticatedScreen';
-import { useFocusEffect } from '@react-navigation/native'; // Import useFocusEffect hook
+import { useFocusEffect } from '@react-navigation/native';
 
 const TrailsScreen = () => {
   const [trailData, setTrailData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
 
-  const checkAuthentication = useCallback(async () => {
-    try {
-      const isAuthenticated = await IsAuthenticated();
-      setAuthenticated(isAuthenticated);
-
-      if (isAuthenticated) {
-        const data = await GetTrails();
-        setTrailData(data);
+  const checkAuthentication = useCallback(() => {
+    IsAuthenticated()
+      .then(isAuthenticated => {
+        setAuthenticated(isAuthenticated);
+        if (isAuthenticated) {
+          return GetTrails();
+        } else {
+          setLoading(false);
+          return Promise.resolve([]);
+        }
+      })
+      .then(data => {
+        if (data.length > 0) {
+          setTrailData(data);
+        }
         setLoading(false);
-      } else {
+      })
+      .catch(error => {
+        console.error('Error fetching trails:', error);
         setLoading(false);
-      }
-    } catch (error) {
-      console.error('Error fetching trails:', error);
-      setLoading(false);
-    }
+      });
   }, []);
 
   useEffect(() => {

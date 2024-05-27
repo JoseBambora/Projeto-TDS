48caf65be9f271dfef50877e21976ee57b0894c1
JoseBambora <zebragapt@gmail.com>
2024-05-27 16:57:50 +0100

    Solved all the conflits.

--- BraGuiaReact/src/components/Emergency.jsx
+++ BraGuiaReact/src/components/Emergency.jsx
@@ -1,7 +1,7 @@
 import OurButton from "./Button"
 import { Linking } from "react-native"
 
-const Emergency = () => <OurButton icon={'call-outline'} title={''} color='white' iconColor='black' onPress={() => Linking.openURL('tel:112')} />
+const Emergency = () => <OurButton icon={'call-outline'} color='white' iconColor='black' onPress={() => Linking.openURL('tel:112')} />
 
 
 export const EmergencyCall = () => {

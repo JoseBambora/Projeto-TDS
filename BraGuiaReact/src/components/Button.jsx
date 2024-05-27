34679122dd6d263b4ae7e5882f0ddb460070b102
JoseBambora <zebragapt@gmail.com>
2024-05-27 16:46:10 +0100

    Undo some changes.

--- BraGuiaReact/src/components/Button.jsx
+++ BraGuiaReact/src/components/Button.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'; // Assuming you're using FontAwesome icons
 import ButtonStyle from '../styles/Button';
-import { buttonColorPrimary, iconsColorPrimary } from '../styles/Colors';
+import { iconsColorPrimary } from '../styles/Colors';
 
 function OurButton({ onPress, title, icon}) {
   return (
     <TouchableOpacity style={ButtonStyle.button} onPress={onPress}>
       <View style={ButtonStyle.buttonContent}>
       {icon && <Ionicons name={icon} size={20} color={iconsColorPrimary()} />}
-        <Text style={ButtonStyle.buttonText}>{title}</Text>
+      <Text style={ButtonStyle.buttonText}>{title}</Text>
       </View>
     </TouchableOpacity>
   );

ab3b10b7f16e55846cd4a98d20e06aca0645cb35
JoseBambora <zebragapt@gmail.com>
2024-05-27 12:44:27 +0100

    Forgot this import.

--- BraGuiaReact/src/components/Emergency.jsx
+++ BraGuiaReact/src/components/Emergency.jsx
@@ -1,4 +1,5 @@
 import OurButton from "./Button"
+import { Linking } from "react-native"
 
 const Emergency = () => <OurButton icon={'call-outline'} title={''} color='white' iconColor='black' onPress={() => Linking.openURL('tel:112')} />
 

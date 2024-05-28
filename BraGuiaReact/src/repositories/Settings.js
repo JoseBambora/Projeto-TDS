de989bea24b607bda50b2b8b4373075b69a6d5af
JoseBambora <zebragapt@gmail.com>
2024-05-28 11:40:19 +0100

    Settings Load and Save.

---
+++ BraGuiaReact/src/repositories/Settings.js
@@ -0,0 +1,9 @@
+import { loadSettingsDB, isDarkModeDB, locationOnDB, notificationOnDB, saveDarkModeDB, saveLocationOnDB, saveNotificationOnDB } from "../redux/Settings"
+
+export const SaveDarkMode = (b) => saveDarkModeDB(b)
+export const SaveLocationOn = (b) => saveLocationOnDB(b)
+export const SaveNotificationOn = (b) => saveNotificationOnDB(b)
+export const GetSettings = () => loadSettingsDB()
+export const IsDarkMode = () => isDarkModeDB()
+export const IsLocationOn = () => locationOnDB()
+export const IsNotificationOn = () => notificationOnDB()

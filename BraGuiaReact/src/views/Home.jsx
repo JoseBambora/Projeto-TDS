72e1971d0d487ece6efeb335b1b95febecc06e72
Miguel Cidade Silva <73348796+MiguelCidadeSilva@users.noreply.github.com>
2024-06-05 17:40:53 +0100

    Custom App logo

--- BraGuiaReact/src/views/Home.jsx
+++ BraGuiaReact/src/views/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Image } from 'react-native';
 import OurText from '../components/ui/Text';
 import OurCardView from '../components/ui/CardView';
 import SocialMedia from '../components/sub-components/SocialMedia';
@@ -10,41 +10,48 @@ import { GetApp } from '../repositories/App';
 import LoadingIndicator from '../components/ui/Indicator';
 import PageStyle from '../styles/ui/Pages';
 import { refreshIfDarkModeChanges } from './utils/RefreshDarkMode';
+import HomeStyle from '../styles/ui/Home';
 
 function makeRequest(setData, setLoading, setContacts) {
   useEffect(() => {
     GetApp()
       .then(json => {
-        setData(json)
-        setContacts({ 'Contactos': json.contacts.map((e) => `${e.contact_name} (${e.contact_phone})`) })
+        setData(json);
+        setContacts({ 'Contactos': json.contacts.map((e) => `${e.contact_name} (${e.contact_phone})`) });
       })
       .catch(err => alert(err.message))
-      .finally(() => setLoading(false))
-  }, [])
+      .finally(() => setLoading(false));
+  }, []);
 }
 
 function Home() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [contacts, setContacts] = useState([])
-  makeRequest(setData, setLoading, setContacts)
-  refreshIfDarkModeChanges()
+  const [contacts, setContacts] = useState([]);
+  makeRequest(setData, setLoading, setContacts);
+  refreshIfDarkModeChanges();
   const content = {
     'Descrição': data.app_desc,
     'Extra': data.app_landing_page_text
-  }
-  return isLoading ? (<LoadingIndicator />)
-    : (
-      <ScrollView style={PageStyle(pageColor()).page}>
-        <OurText content={data.app_name} fontSize={30} color={textColorHeader()} textAlign={'center'} />
-        <OurCardView data={content} />
-        <OurCardView data={contacts} />
-        <View style={FooterStyle.footer}>
-          <SocialMedia social_links={data.socials} />
-          <Partners partners={data.partners} />
-        </View>
-      </ScrollView>
-    )
-};
+  };
+
+  return isLoading ? (
+    <LoadingIndicator />
+  ) : (
+    <ScrollView style={PageStyle(pageColor()).page}>
+      <OurText content={data.app_name} fontSize={30} color={textColorHeader()} textAlign={'center'} />
+      <Image
+        source={require('../../android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_foreground.png')}
+        style={HomeStyle.logo}
+      />
+      <OurCardView data={content} />
+      <OurCardView data={contacts} />
+      <View style={FooterStyle.footer}>
+        <SocialMedia social_links={data.socials} />
+        <Partners partners={data.partners} />
+      </View>
+    </ScrollView>
+  );
+}
 
 export default Home;

3921ccbc6fa3e24490b7bf6c5eb833f51b22ce0b
zeisalone <jmbarbosa2002@gmail.com>
2024-06-07 13:25:18 +0100

    History+darkmode upd

--- BraGuiaReact/src/components/sub-components/HistoryComponent.jsx
+++ BraGuiaReact/src/components/sub-components/HistoryComponent.jsx
@@ -7,23 +7,32 @@ import { pageColor } from '../../styles/Colors';
 import { useNavigation } from '@react-navigation/native';
 
 const HistoryComponent = ({ trailsData, pointsOfInterestData }) => {
-
   const groupByDate = (data, itemType, page, navigation) => {
     const groupedData = {};
+    const options = { weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric' };
+
     data.forEach((item) => {
-      const date = new Date(item.date).toLocaleDateString();
+      const parts = new Intl.DateTimeFormat('pt-PT', options).formatToParts(new Date(item.date));
+      const dateParts = {};
+      parts.forEach(part => dateParts[part.type] = part.value);
+      const capitalizedWeekday = dateParts.weekday.charAt(0).toUpperCase() + dateParts.weekday.slice(1);
+      const date = `${capitalizedWeekday}, ${dateParts.day}/${dateParts.month}/${dateParts.year}`;
+
       if (!groupedData[date]) {
         groupedData[date] = [];
       }
-      const id = item.id
+      const id = item.id;
       groupedData[date].push([item[itemType], () => navigation.navigate(page, { id })]);
     });
+
     return groupedData;
   };
+
   const navigation = useNavigation();
   const groupedTrailsData = groupByDate(trailsData, 'trailName', 'TrailDetail', navigation);
   const groupedPointsOfInterestData = groupByDate(pointsOfInterestData, 'pointOfInterestName', 'PinDetail', navigation);
   const HistoryStyleVar = HistoryStyles(pageColor)
+  
   return (
     <ScrollView style={HistoryStyleVar.container}>
       <View style={HistoryStyleVar.section}>
